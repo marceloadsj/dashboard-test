7dfe1d75164afe0aedf542ce042e091243560e11
Marcelo Junior <marceloadsj1@gmail.com>
2020-03-01 14:33:59 +0100

    feature: added menu on header to navigate between pages

--- src/domains/app/Header.js
+++ src/domains/app/Header.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Layout, Row, Col, Dropdown, Menu, Avatar, Typography } from "antd";
 import { useObserver } from "mobx-react-lite";
-import { LogoutOutlined } from "@ant-design/icons";
+import {
+  LogoutOutlined,
+  LineChartOutlined,
+  TableOutlined
+} from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "images/logo.svg";
 import useUserStore from "domains/user/useUserStore";
 
 const avatarStyle = { backgroundColor: "#1890ff" };
+const defaultSelectedKeys = ["dashboard"];
 
 export default function Header() {
   const userStore = useUserStore();
 
   const onLogoutClick = () => userStore.logout();
 
-  const menu = (
+  const userMenu = (
     <Menu>
       <Menu.Item onClick={onLogoutClick}>
         <LogoutOutlined /> Logout
@@ -24,23 +30,46 @@ export default function Header() {
   return useObserver(() => (
     <Layout.Header>
       <Row justify="space-between" className="h-full">
-        <Col className="flex items-center">
-          <Logo className="text-white w-30 mr-10" />
+        <Col className="flex">
+          <div className="h-full items-center mr-50 hidden md:flex">
+            <Logo className="text-white w-30 mr-10" />
 
-          <Typography.Title level={4} className="text-white mb-0">
-            {process.env.REACT_APP_NAME}
-          </Typography.Title>
+            <Typography.Title level={4} className="text-white mb-0">
+              {process.env.REACT_APP_NAME}
+            </Typography.Title>
+          </div>
+
+          {userStore.userIsLogged && (
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={defaultSelectedKeys}
+              className="line-h-64"
+            >
+              <Menu.Item key="dashboard">
+                <Link to="/">
+                  <LineChartOutlined /> Dashboard
+                </Link>
+              </Menu.Item>
+
+              <Menu.Item key="list">
+                <Link to="/list">
+                  <TableOutlined /> List
+                </Link>
+              </Menu.Item>
+            </Menu>
+          )}
         </Col>
 
         {userStore.userIsLogged && (
           <Col>
-            <Dropdown overlay={menu}>
+            <Dropdown overlay={userMenu}>
               <div className="cursor-pointer">
                 <Avatar style={avatarStyle} className="mr-10">
                   {userStore.loggedUserInitials}
                 </Avatar>
 
-                <span className="text-white font-bold">
+                <span className="text-white font-bold hidden md:inline">
                   {userStore.loggedUser.name}
                 </span>
               </div>
