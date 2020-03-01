c33e9e9ebb36b730fadb324736954cacee9f26fc
Marcelo Junior <marceloadsj1@gmail.com>
2020-03-01 11:35:57 +0100

    improvement: show a loading indicator while loading user info after login

--- src/domains/user/LoginPage.js
+++ src/domains/user/LoginPage.js
@@ -16,8 +16,10 @@ import {
   LoginOutlined,
   UserOutlined,
   LockOutlined,
-  UserAddOutlined
+  UserAddOutlined,
+  LoadingOutlined
 } from "@ant-design/icons";
+import { useObserver } from "mobx-react-lite";
 
 import { ReactComponent as Logo } from "images/logo.svg";
 import useUserStore from "./useUserStore";
@@ -29,6 +31,8 @@ const usernameRules = [
 const passwordRules = [
   { required: true, message: "Please input your password" }
 ];
+
+const loadingIconStyle = { fontSize: 100 };
 
 export default function LoginPage() {
   const userStore = useUserStore();
@@ -56,95 +60,105 @@ export default function LoginPage() {
     }
   }
 
-  return (
+  return useObserver(() => (
     <Row justify="center" className="mt-50">
       <Col xs={23} sm={19} md={14} lg={11} xl={9} xxl={7}>
-        <Card className="shadow mb-50">
-          <Form
-            labelCol={{ span: 6 }}
-            wrapperCol={{ span: 18 }}
-            onFinish={onFinish}
-          >
-            <div className="text-center mb-50">
-              <Logo className="w-100 text-blue-500 -mt-50 pb-10" />
-
-              <Typography.Title level={3}>
-                Welcome to {process.env.REACT_APP_NAME}
-              </Typography.Title>
-
-              <Typography.Paragraph className="font-bold">
-                Here you can have full control of your data and settings
-              </Typography.Paragraph>
-            </div>
-
-            <Form.Item
-              label={<Tooltip title="Username or Email">Username</Tooltip>}
-              name="username"
-              rules={usernameRules}
-              className="pb-10"
-            >
-              <Input
-                disabled={loading}
-                size="large"
-                placeholder="E.g john_doe or john.doe@example.com"
-                prefix={<UserOutlined />}
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={passwordRules}
-              className="pb-10"
-            >
-              <Input.Password
-                disabled={loading}
-                size="large"
-                placeholder="E.g your secret password here"
-                autoComplete="password"
-                prefix={<LockOutlined />}
-              />
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ span: 24 }}>
-              <Button
-                type="primary"
-                htmlType="submit"
-                size="large"
-                block
-                icon={<LoginOutlined />}
-                loading={loading}
-              >
-                Log into my Account
-              </Button>
-            </Form.Item>
-
-            <Divider>or</Divider>
-
-            <Link
-              to="/register"
-              component={LoginPageRegisterButton}
-              appearance="ghost"
-              block
-              type="dashed"
-              icon={<UserAddOutlined />}
-            >
-              Register a new Account
-            </Link>
-          </Form>
-        </Card>
-
-        <Typography.Paragraph className="opacity-50 text-center text-sm">
-          For testing purposes, you can login with:
-          <div className="font-bold">
-            Username: demo_user or Email: demo@demo.com
-            <br />
-            Password: demo
+        {userStore.userHasToken && (
+          <div className="text-center">
+            <LoadingOutlined style={loadingIconStyle} />
           </div>
-        </Typography.Paragraph>
+        )}
+
+        {!userStore.userHasToken && (
+          <>
+            <Card className="shadow mb-50">
+              <Form
+                labelCol={{ span: 6 }}
+                wrapperCol={{ span: 18 }}
+                onFinish={onFinish}
+              >
+                <div className="text-center mb-50">
+                  <Logo className="w-100 text-blue-500 -mt-50 pb-10" />
+
+                  <Typography.Title level={3}>
+                    Welcome to {process.env.REACT_APP_NAME}
+                  </Typography.Title>
+
+                  <Typography.Paragraph className="font-bold">
+                    Here you can have full control of your data and settings
+                  </Typography.Paragraph>
+                </div>
+
+                <Form.Item
+                  label={<Tooltip title="Username or Email">Username</Tooltip>}
+                  name="username"
+                  rules={usernameRules}
+                  className="pb-10"
+                >
+                  <Input
+                    disabled={loading}
+                    size="large"
+                    placeholder="E.g john_doe or john.doe@example.com"
+                    prefix={<UserOutlined />}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={passwordRules}
+                  className="pb-10"
+                >
+                  <Input.Password
+                    disabled={loading}
+                    size="large"
+                    placeholder="E.g your secret password here"
+                    autoComplete="password"
+                    prefix={<LockOutlined />}
+                  />
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ span: 24 }}>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    size="large"
+                    block
+                    icon={<LoginOutlined />}
+                    loading={loading}
+                  >
+                    Log into my Account
+                  </Button>
+                </Form.Item>
+
+                <Divider>or</Divider>
+
+                <Link
+                  to="/register"
+                  component={LoginPageRegisterButton}
+                  appearance="ghost"
+                  block
+                  type="dashed"
+                  icon={<UserAddOutlined />}
+                >
+                  Register a new Account
+                </Link>
+              </Form>
+            </Card>
+
+            <Typography.Paragraph className="opacity-50 text-center text-sm">
+              For testing purposes, you can login with:
+              <div className="font-bold">
+                Username: demo_user or Email: demo@demo.com
+                <br />
+                Password: demo
+              </div>
+            </Typography.Paragraph>
+          </>
+        )}
       </Col>
     </Row>
-  );
+  ));
 }
 
 function LoginPageRegisterButton({ navigate, ...props }) {
